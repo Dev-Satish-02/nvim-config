18ac062c19d9fd0ee9561b117857d25387937fd8
Dev-Satish-02 <devsatish08@gmail.com>
2026-03-26 22:36:29 +0530

    bug fix

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -9,6 +9,17 @@ return {
         sidebars = "transparent",
         floats = "transparent",
       },
+      on_highlights = function(hl, c)
+        hl.VertSplit = { fg = c.bg, bg = c.bg }
+        hl.WinSeparator = { fg = c.bg, bg = c.bg } -- IMPORTANT (new nvim uses this)
+      end,
     },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd("colorscheme tokyonight")
+
+      -- force override AFTER colorscheme
+      vim.opt.fillchars = { vert = " " }
+    end,
   },
 }
